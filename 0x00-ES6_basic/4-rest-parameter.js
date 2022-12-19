e3321104ef22405ddaacfeb2b29bdacb94cd7404
SYLLA Yacouba <sylyac2000@gmail.com>
2022-12-19 14:48:22 +0000

    4. Rest parameter syntax for functions maj

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,4 +1,3 @@
-export default function returnHowManyArguments(...restparameters) 
-{
-	return restparameters.length;
+export default function returnHowManyArguments(...restparameters) {
+  return restparameters.length;
 }
