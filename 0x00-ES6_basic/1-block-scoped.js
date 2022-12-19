04032f97c0ebec137a97322727b13d2ff25486f3
SYLLA Yacouba <sylyac2000@gmail.com>
2022-12-19 18:14:12 +0000

    1. Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,8 @@
 export default function taskBlock(trueOrFalse) {
+  /* eslint no-shadow: "error" */
   const task = false;
   const task2 = true;
-
+  /*eslint-disable */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
