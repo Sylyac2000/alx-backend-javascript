5398fb797dbbce6210db9b0e007aa90e7b816430
SYLLA Yacouba <sylyac2000@gmail.com>
2022-12-22 11:03:13 +0000

    5. A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+// Abstract class for a building
+export default class Building {
+  // Constructor
+  constructor(sqft) {
+    // Store the attribute in an "underscore" attribute
+	if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  // Getter for the sqft attribute
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Abstract method that must be implemented by any class that extends from Building
+  /*evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }*/
+}
