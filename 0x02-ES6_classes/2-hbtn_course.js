3ed9fdb17cb3784d711055d95103520859f7693f
SYLLA Yacouba <sylyac2000@gmail.com>
2022-12-22 16:52:40 +0000

    2. A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,51 +1,49 @@
 export default class HolbertonCourse {
-	
-	constructor(name, length, students) {
-		
-		if (typeof name !== 'string') {
-			throw new Error('Name must be a string');
-		}
-		if (!Number.isInteger(length)) {
-			throw new Error('Length must be a number');
-		}
-		if (!Array.isArray(students) || students.some(s => typeof s !== 'string')) {
-			throw new Error('students must be an array of strings');
-		}
-		this._name = name;
-		this._length = length;
-		this._students = students;
-	}
-	
-	get name() {
-		return this._name;
-	}
-	set name(value) {
-		if (typeof value !== 'string') {
-			throw new Error('name must be a string');
-		}
-		this._name = value;
-	}
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    if (!Number.isInteger(length)) {
+      throw new Error('Length must be a number');
+    }
+    if (!Array.isArray(students) || students.some((s) => typeof s !== 'string')) {
+      throw new Error('students must be an array of strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
 
-	get length() {
-		return this._length;
-	}
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new Error('name must be a string');
+    }
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
 
   set length(value) {
     if (!Number.isInteger(value)) {
-		throw new Error('length must be a number');
+      throw new Error('length must be a number');
     }
     this._length = value;
   }
-  
+
   get students() {
-	return this._students;
+    return this._students;
   }
 
   set students(value) {
-    if (!Array.isArray(value) || value.some(s => typeof s !== 'string')) {
-		throw new Error('students must be an array of strings');
+    if (!Array.isArray(value) || value.some((s) => typeof s !== 'string')) {
+      throw new Error('students must be an array of strings');
     }
     this._students = value;
   }
-
 }
