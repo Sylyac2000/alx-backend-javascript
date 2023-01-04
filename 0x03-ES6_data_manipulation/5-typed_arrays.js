bcca5ca9525c807e19690b20e4a9ff40375bc453
SYLLA Yacouba <sylyac2000@gmail.com>
2023-01-04 04:49:31 +0000

    5. Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,8 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
 
-  const arrayBuffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(arrayBuffer);
-  int8View[position] = value;
-  return arrayBuffer;
+  const buffer = new DataView(new ArrayBuffer(length), 0, length);
+  buffer.setInt8(position, value);
+  return buffer;
 }
