9d38e1a5d8d80334be51a19eced02ce58cdcf5bb
SYLLA Yacouba <sylyac2000@gmail.com>
2023-02-21 06:17:31 +0000

    1. Using Process stdin upda

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,12 @@
-process.stdin.setEncoding('utf8');
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-process.stdin.on('data', (data) => {
-  const name = data.trim();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
 
   if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
+    process.stdout.write(`Your name is: ${name}`);
   }
-
-  process.stdin.pause();
 });
 
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
-process.stdin.resume();
